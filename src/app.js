67f4504a3de3a6fe43de46fc18596b601668d1bb
Huey <leejinhuey@gmail.com>
2020-01-19 11:12:08 +0000

    add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
-
+import cors from 'cors'
 import api from './api'
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
+
+app.use(cors())
 
 app.get(`/`, (req, res) => res.send(`See the github: https://github.com/hueyy/unofficial-apis`))
 
